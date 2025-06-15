e1678f7f0ea3488a2bd7cf0071103976037348db
bouydia <younessbouydia@gmail.com>
2025-06-15 09:13:17 +0100

    add console.log

--- models/expense/Expense.js
+++ models/expense/Expense.js
@@ -1,6 +1,15 @@
-import { getAuth } from "firebase/auth";
-import { collection, addDoc, Timestamp, query, where, getDocs, orderBy, limit } from "firebase/firestore";
-import { app, db } from "../../firebase"; 
+import { getAuth } from 'firebase/auth';
+import {
+  collection,
+  addDoc,
+  Timestamp,
+  query,
+  where,
+  getDocs,
+  orderBy,
+  limit,
+} from 'firebase/firestore';
+import { app, db } from '../../firebase';
 
 class Expense {
   user_id;
@@ -15,18 +24,28 @@ class Expense {
     this.title = title;
     this.amount = parseFloat(amount);
     this.category = category;
-    this.description = note || "";
+    this.description = note || '';
     this.user_id = null;
     this.incurred_at = null;
     this.group_id = group_id; // Optional, can be set later if needed
+
+    console.log('New Expense Created:', {
+      title: this.title,
+      amount: this.amount,
+      category: this.category,
+      description: this.description,
+      user_id: this.user_id,
+      incurred_at: this.incurred_at,
+      group_id: this.group_id,
+    });
   }
 
   validate() {
     if (!this.title || !this.amount || !this.category) {
-      throw new Error("All fields (name, amount, category) are required");
+      throw new Error('All fields (name, amount, category) are required');
     }
     if (isNaN(this.amount) || this.amount <= 0) {
-      throw new Error("Amount must be a positive number");
+      throw new Error('Amount must be a positive number');
     }
     return true;
   }
@@ -37,7 +56,7 @@ class Expense {
     const auth = getAuth(app);
     const currentUser = auth.currentUser;
     if (!currentUser) {
-      throw new Error("No authenticated user found");
+      throw new Error('No authenticated user found');
     }
     this.user_id = currentUser.uid;
 
@@ -53,7 +72,7 @@ class Expense {
       group_id: this.group_id || null,
     };
 
-    const expensesCollection = collection(db, "expenses"); // Reference to the "expenses" collection
+    const expensesCollection = collection(db, 'expenses'); // Reference to the "expenses" collection
     const docRef = await addDoc(expensesCollection, expenseData); // Add document to the collection
     return docRef.id;
   }
@@ -61,57 +80,54 @@ class Expense {
   static async GetAllExpenseByUser() {
     const auth = getAuth(app);
     const currentUser = auth.currentUser;
-  
+
     if (!currentUser) {
-      throw new Error("No authenticated user found");
+      throw new Error('No authenticated user found');
     }
-  
-    const expensesCollection = collection(db, "expenses");
-    const q = query(expensesCollection, where("user_id", "==", currentUser.uid));
-  
+
+    const expensesCollection = collection(db, 'expenses');
+    const q = query(expensesCollection, where('user_id', '==', currentUser.uid));
+
     const snapshot = await getDocs(q);
-  
-    const expenses = snapshot.docs.map(doc => ({
+
+    const expenses = snapshot.docs.map((doc) => ({
       id: doc.id,
       ...doc.data(),
     }));
-  
+
     return expenses;
   }
 
   static async RecentlyActivityByUser() {
     const auth = getAuth(app);
     const currentUser = auth.currentUser;
-  
+
     if (!currentUser) {
-      throw new Error("No authenticated user found");
+      throw new Error('No authenticated user found');
     }
-  
-    const expensesCollection = collection(db, "expenses");
-  
+
+    const expensesCollection = collection(db, 'expenses');
+
     const q = query(
       expensesCollection,
-      where("user_id", "==", currentUser.uid),
-      orderBy("incurred_at", "desc"),
+      where('user_id', '==', currentUser.uid),
+      orderBy('incurred_at', 'desc'),
       limit(3)
     );
-  
+
     const snapshot = await getDocs(q);
-  
-    const recentExpenses = snapshot.docs.map(doc => ({
+
+    const recentExpenses = snapshot.docs.map((doc) => ({
       id: doc.id,
       ...doc.data(),
     }));
-  
+
     return recentExpenses;
   }
 
   // function that return expenses by group_id limit 3
 
   // function that return expenses by group_id all
-
-
-  
 }
 
 export default Expense;
